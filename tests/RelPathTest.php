4c864e67202da816a376b2c6a2527a0d567f0c39
Timo Tijhof <krinkle@fastmail.com>
2025-03-13 19:28:20 -0700

    tests: Add test cases
    Bug: T388323
    Change-Id: I32fb8b80c208ce32102f89609df9fb4964cb2e70

--- tests/RelPathTest.php
+++ tests/RelPathTest.php
@@ -35,70 +35,90 @@ class RelPathTest extends TestCase {
 	public function testSplitPath() {
 		$this->assertEquals(
 			[ '', 'foo', 'bar', 'baz.txt' ],
-			RelPath::splitPath( '/foo/bar/baz.txt' )
+			RelPath::splitPath( '/foo/bar/baz.txt' ),
+			'simple'
 		);
 
 		$this->assertEquals(
 			[ '', 'foo', 'bar', 'baz.txt' ],
-			RelPath::splitPath( '/foo/bar/./baz.txt' )
+			RelPath::splitPath( '/foo/bar/./baz.txt' ),
+			'dot'
 		);
 
 		$this->assertEquals(
 			[ '', 'foo', 'baz.txt' ],
-			RelPath::splitPath( '/foo/bar/../baz.txt' )
+			RelPath::splitPath( '/foo/bar/../baz.txt' ),
+			'dotdot'
 		);
 	}
 
 	public function provideRelPathTestCases() {
 		return [
-			[ '/foo/bar/', '/foo/bar/baz/', '..' ],
-			[ '/foo/bar/', '/foo/bar/baz.txt', '..' ],
-			[ '/foo/bar/', '/foo/bar/', '.' ],
-			[ '/foo/bar/', '/foo/bar/', '.' ],
-			[ '/', '/foo/', '..' ],
-			[ '/', '/foo/bar.txt', '../..' ],
-			[ '/', '/foo/bar/baz.txt', '../../..' ],
-			[ '/foo/bar/', '/foo/baz/', '../bar' ],
-			[ '/foo/bar/', '/foo/baz.txt', '../bar' ],
-			[ '/foo/bar/baz/', '/foo/bar/', 'baz' ],
-			[ '/foo/bar/baz', '/foo/bar', 'baz' ],
-			[ '/foo/bar.txt', '/foo/baz/', '../bar.txt' ],
-			[ '/foo/bar.txt', '/foo/baz.txt', '../bar.txt' ],
-			[ '/foo/bar', '/foo/baz/', '../bar' ],
-			[ '/foo/bar', '/foo/baz.txt', '../bar' ],
-			[ '/foo/bar', '/foo/bar/baz/', '..' ],
-			[ '/foo/bar', '/foo/bar/baz.txt', '..' ],
-			[ '/foo/bar/bat', '/x/y/z', '../../../foo/bar/bat' ],
-			[ '/foo/bar/bat', '/foo/bar', 'bat' ],
-			[ '/foo/bar/bat', '/', 'foo/bar/bat' ],
-			[ '/', '/foo/bar/bat', '../../..' ],
-			[ '/foo/bar/bat', '/x', '../foo/bar/bat' ],
-			[ '/x', '/foo/bar/bat', '../../../x' ],
-			[ '/', '/', '.' ],
-			[ '/a', '/a', '.' ],
-			[ '/a/b', '/a/b', '.' ],
-			[ 'foo/bar', 'quux', false ],
-			[ '/foo\\bar/', '/foo\\bar/baz/', '..' ],
+			'parent slash from subdir'      => [ '/foo/bar/', '/foo/bar/baz/', '..' ],
+			'parent noslash from subdir'    => [ '/foo/bar', '/foo/bar/baz/', '..' ],
+			'parent slash from file'        => [ '/foo/bar/', '/foo/bar/baz.txt', '..' ],
+			'parent noslash from file'      => [ '/foo/bar', '/foo/bar/baz.txt', '..' ],
+			'parent containing backslash'   => [ '/foo\\bar/', '/foo\\bar/baz/', '..' ],
+			'root from subdir'              => [ '/', '/foo/', '..' ],
+			'root from file'                => [ '/', '/foo/bar.txt', '../..' ],
+			'root from nested file'         => [ '/', '/foo/bar/baz.txt', '../../..' ],
+			'root from nested dir'          => [ '/', '/foo/bar/bat', '../../..' ],
+			'sibling dir slash'             => [ '/foo/bar/', '/foo/baz/', '../bar' ],
+			'sibling dir slash from file'   => [ '/foo/bar/', '/foo/baz.txt', '../bar' ],
+			'subdir from parent slash'      => [ '/foo/bar/baz/', '/foo/bar/', 'baz' ],
+			'subdir from parent noslash'    => [ '/foo/bar/baz', '/foo/bar', 'baz' ],
+			'sibling dir file from dir'     => [ '/foo/bar.txt', '/foo/baz/', '../bar.txt' ],
+			'sibling dir file from file'    => [ '/foo/bar.txt', '/foo/baz.txt', '../bar.txt' ],
+			'sibling dir noslash'           => [ '/foo/bar', '/foo/baz/', '../bar' ],
+			'sibling dir noslash from file' => [ '/foo/bar', '/foo/baz.txt', '../bar' ],
+			'via root deep from deep'       => [ '/foo/bar/bat', '/x/y/z', '../../../foo/bar/bat' ],
+			'via root deep from sub'        => [ '/foo/bar/bat', '/x', '../foo/bar/bat' ],
+			'via root sub from deep'        => [ '/x', '/foo/bar/bat', '../../../x' ],
+			'nested from root'              => [ '/foo/bar/bat', '/', 'foo/bar/bat' ],
+			'noop root'                     => [ '/', '/', '.' ],
+			'noop subdir'                   => [ '/a', '/a', '.' ],
+			'noop nested'                   => [ '/a/b', '/a/b', '.' ],
+			'base invalid'                  => [ 'foo/bar', 'quux', false ],
+			// T388323
+			// 'intermediate two'              => [ '/a/bat/cat/dog/boy/../../assets/img.png',
+			// 	'/a/bat/cat', 'assets/img.png'
+			// ],
+			// 'intermediate three'            => [ '/a/bat/cat/dog/boy/../../../assets/x.txt',
+			// 	'/a/bat', 'assets/x.txt'
+			// ],
+			// 'intermediate overflow'         => [ '/a/bat/cat/dog/../../../../eagle/x.txt',
+			// 	'/a/bat', '../../eagle/x.txt'
+			// ],
+			// 'intermediate back and forth'   => [
+			// 	'/a/bat/cat/dog/eagle/egg1/../egg2/../egg3/../../../assets/x.txt',
+			// 	'/a/bat', 'cat/assets/x.txt'
+			// ],
 		];
 	}
 
 	public function provideJoinPathTestCases() {
 		return [
-			[ '/foo/bar', './baz', '/foo/bar/baz' ],
-			[ '/foo/bar', '/tmp/file/', '/tmp/file/' ],
-			[ '/foo/0/bar', '/tmp/0/file/', '/tmp/0/file/' ],
-			[ '/foo/./bar', '/tmp/0/.file/', '/tmp/0/.file/' ],
-			[ '/foo/./bar', 'tmp/0/.file/', '/foo/bar/tmp/0/.file' ],
-			[ '/foo/bar', 'file', '/foo/bar/file' ],
-			[ '/foo/bar', '.file', '/foo/bar/.file' ],
-			[ '/foo//bar', '../baz', '/foo/baz' ],
-			[ '/foo/bar', '../././baz/.', '/foo/baz' ],
-			[ '/foo//bar', '../../baz', '/baz' ],
-			[ '/foo//bar', '../../../baz', '/baz' ],
-			[ '/', '../../../baz', '/baz' ],
-			[ '/foo/bar/../baz', 'quux', '/foo/baz/quux' ],
-			[ '/foo/bar', '../quux/../baz.txt', '/foo/baz.txt' ],
-			[ 'foo/bar', 'quux', false ],
+			'dot relative'                    => [ '/foo/bar', './baz', '/foo/bar/baz' ],
+			'absolute'                        => [ '/foo/bar', '/tmp/file/', '/tmp/file/' ],
+			'zero'                            => [ '/foo/0/bar', '/tmp/0/file/', '/tmp/0/file/' ],
+			'absolute combo dot zero dotfile' => [ '/foo/./bar', '/tmp/0/.file/', '/tmp/0/.file/' ],
+			'relative combo dot zero dotfile' => [ '/foo/./bar', 'tmp/0/.file/', '/foo/bar/tmp/0/.file' ],
+			'solo filename'                   => [ '/foo/bar', 'file', '/foo/bar/file' ],
+			'solo dotfile'                    => [ '/foo/bar', '.file', '/foo/bar/.file' ],
+			'one parent'                      => [ '/foo/bar', '../baz', '/foo/baz' ],
+			'two parents '                    => [ '/foo/bar', '../../baz', '/baz' ],
+			'double-slash parent'             => [ '/foo//bar', '../baz', '/foo/baz' ],
+			'double-slash two parents'        => [ '/foo//bar', '../../baz', '/baz' ],
+			'double-slash many parents'       => [ '/foo//bar/baz//quux', '../../../baz', '/foo/baz' ],
+			'noop dots'                       => [ '/foo/bar', '../././baz/.', '/foo/baz' ],
+			'ignore too far by one'           => [ '/foo/bar', '../../../baz', '/baz' ],
+			'ignore too far by two'           => [ '/foo/bar', '../../../../baz', '/baz' ],
+			'ignore too far from root'        => [ '/', '../../../baz', '/baz' ],
+			'back and forth'                  => [ '/foo/bar/baz/quux',
+				'../../back/under/../../../file.txt', '/foo/file.txt'
+			],
+			'base with dotdot'                => [ '/foo/bar/../baz', 'quux', '/foo/baz/quux' ],
+			'base invalid'                    => [ 'foo/bar', 'quux', false ],
 		];
 	}
 
