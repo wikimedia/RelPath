30b91ffa80da51db8045c9ef903781076c71ee95
Ori Livneh <ori.livneh@gmail.com>
2025-12-21 14:49:07 -0500

    build: Add benchmark for RelPath::joinPath
    On Wikimedia's infrastructure, this function (and its callees) account
    for approximately 2% of the wall-clock time of load.php requests.
    
    Change-Id: I91c39084712a138bec37c547920611d80c1549d8

---
+++ benchmarks/RelPathBench.php
@@ -0,0 +1,25 @@
+<?php
+declare( strict_types = 1 );
+
+// Benchmark for RelPath::joinPath().
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+use Wikimedia\RelPath;
+
+$iterations = 1000;
+
+$base = '/data/example/resources/lib';
+$rel = '../.././resources/lib/foo/foo.js';
+
+$startTime = hrtime( true );
+
+for ( $i = 0; $i < $iterations; $i++ ) {
+	RelPath::joinPath( $base, $rel );
+}
+
+$endTime = hrtime( true );
+$totalTimeMs = ( $endTime - $startTime ) / 1_000_000;
+$avgTimeMs = $totalTimeMs / $iterations;
+
+echo "RelPath::joinPath average time: " . sprintf( '%.4f', $avgTimeMs ) . " ms\n";
