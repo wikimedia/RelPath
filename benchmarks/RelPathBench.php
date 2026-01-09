f38733339734439336d92f0fea3637a59ab5cca6
Ori Livneh <ori.livneh@gmail.com>
2026-01-09 13:15:12 -0500

    build: Add benchmark for RelPath::getRelativePath
    Also increase the iteration count to 100,000 for more precise
    measurements.
    
    Change-Id: If783b60f985ace1fc57600325bc893d90201fe92

--- benchmarks/RelPathBench.php
+++ benchmarks/RelPathBench.php
@@ -1,14 +1,18 @@
 <?php
 declare( strict_types = 1 );
 
-// Benchmark for RelPath::joinPath().
+// Benchmark for RelPath::joinPath() and RelPath::getRelativePath().
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
 use Wikimedia\RelPath;
 
-$iterations = 1000;
+$iterations = 100_000;
 
+echo "PHP " . PHP_VERSION . "\n";
+echo "Iterations: " . number_format( $iterations ) . "\n\n";
+
+// Benchmark joinPath
 $base = '/data/example/resources/lib';
 $rel = '../.././resources/lib/foo/foo.js';
 
@@ -22,5 +26,20 @@ $endTime = hrtime( true );
 $totalTimeMs = ( $endTime - $startTime ) / 1_000_000;
 $avgTimeMs = $totalTimeMs / $iterations;
 
-echo "PHP " . PHP_VERSION . "\n";
-echo "RelPath::joinPath average time: " . sprintf( '%.4f', $avgTimeMs ) . " ms\n";
+echo sprintf( "%-40s %.6f ms\n", "RelPath::joinPath average time:", $avgTimeMs );
+
+// Benchmark getRelativePath
+$path = '/data/example/resources/lib/foo/foo.js';
+$start = '/data/example/resources/src';
+
+$startTime = hrtime( true );
+
+for ( $i = 0; $i < $iterations; $i++ ) {
+	RelPath::getRelativePath( $path, $start );
+}
+
+$endTime = hrtime( true );
+$totalTimeMs = ( $endTime - $startTime ) / 1_000_000;
+$avgTimeMs = $totalTimeMs / $iterations;
+
+echo sprintf( "%-40s %.6f ms\n", "RelPath::getRelativePath average time:", $avgTimeMs );
